d47fd2157247c52923235c116084f85e6c23df31
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2020-03-17 13:45:18 +0100

    Create projet.py

---
+++ projet.py
@@ -0,0 +1,6 @@
+Projet
+
+Le but de mon projet est de compter le nombre de colis qui peuvent rentrer dans un ascenseur. Sachant qu'il existe cinq types de colis :
+enveloppe, petit, moyen, grand et très grand. Ils ont comme poids respectif 0.5, 1, 3, 5 et 10 kilos. Le poids maximal autorisé dans
+l'ascenseur est de 25 kilos. Nous voulons donc savoir combien de colis peuvent y rentrer mais aussi combien de chaque catégorie.
+Pour ce faire, je vais réaliser un code python.
