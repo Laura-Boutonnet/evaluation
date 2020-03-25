556d197da43cee12a73a9f8c305d83471a7c1326
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2020-03-25 14:16:20 +0100

    Update test_fichier.py

--- test_fichier.py
+++ test_fichier.py
@@ -1,1 +1,33 @@
 from fichier import categorie
+
+class MyTestCase(unittest.TestCase):
+    def test_prendre_colis(self):
+        enveloppe, petit, moyen, grand, enorme, petit, enveloppe, enorme = [
+                creation_colis(categorie) for categorie in 
+                ['enveloppe', 'petit', 'moyen', 'grand', 'enorme', 'petit', 'envloppe', 'enorme']]
+        ascenceur = {'categories': [], 'poids_max': 25}
+        self.assertEqual(prendre_categorie(ascenceur, enveloppe), True)
+        self.assertTrue(prendre_categorie(ascenceur, petit))
+        self.assertTrue(prendre_categorie(ascenceur, moyen))
+        self.assertTrue(prendre_categorie(ascenceur, grand))
+        self.assertTrue(prendre_categorie(ascenceur, enorme))
+        self.assertTrue(prendre_categorie(ascenceur, petit))
+        self.assertTrue(prendre_categorie(ascenceur, enveloppe))
+        self.assertFalse(prendre_categorie(ascenceur, enorme), f"Nous ne pouvons pas accepter un colis de cette categorie car le poids du colis est suppérieur au poids libre restant dans l'ascenceur")
+    
+    def test_final_colis_poids(self):
+        enveloppe, petit, moyen, grand, enorme, petit, enveloppe, enorme = [
+                creation_colis(categorie) for categorie in 
+                ['enveloppe', 'petit', 'moyen', 'grand', 'enorme', 'petit', 'envloppe', 'enorme']]
+        ascenceur = {'categories': [], 'poids_max': 25}
+        self.assertEqual(prendre_categorie(ascenceur, enveloppe), True)
+        self.assertTrue(prendre_categorie(ascenceur, petit))
+        self.assertTrue(prendre_categorie(ascenceur, moyen))
+        self.assertTrue(prendre_categorie(ascenceur, grand))
+        self.assertTrue(prendre_categorie(ascenceur, enorme))
+        self.assertTrue(prendre_categorie(ascenceur, petit))
+        self.assertTrue(prendre_categorie(ascenceur, enveloppe))
+        self.assertFalse(prendre_categorie(ascenceur, enorme), f"Nous ne pouvons pas accepter un colis de cette categorie car le poids du colis est suppérieur au poids libre restant dans l'ascenceur")
+    
+        final_colis_poids(ascenceur)
+        self.assertFalse(ascenceur['categories'])
