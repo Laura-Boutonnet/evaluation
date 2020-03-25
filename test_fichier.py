8dea5f1c73d3d17f5413f299e540d62048d79da8
Laura-Boutonnet <laura.boutonnet@hotmail.fr>
2020-03-25 20:26:26 +0000

    Continuation des tests

--- test_fichier.py
+++ test_fichier.py
@@ -17,42 +17,41 @@ class MyTestCase(unittest.TestCase):
                 "enorme",
             ]
         ]
-        self.ascenceur = {"categories": [], "poids_max": 25}
+        self.ascenceur = {"colis": [], "poids_max": 25}
+        self.lot = creation_colis2(['enveloppe', 'petit', 'moyen', 'grand','enorme','petit','enveloppe','enorme'])
+        self.lot3enormes = creation_colis2(["enorme"] * 3)
 
     def test_prendre_lot_enorme(self):
-        ascenceur = {"colis": [], "poids_max": 25}
-        lot3enormes = creation_colis2(["enorme"] * 3)
+        ascenceur = self.ascenceur
+        lot3enormes = self.lot3enormes
         prises = [prendre_colis(ascenceur, colis) for colis in lot3enormes]
         self.assertEqual(prises, [True, True, False])
         self.assertEqual(ascenceur["colis"], lot3enormes[:2])
-
     
     def test_prendre_colis(self):
-        ascenceur = {'colis': [], 'poids_max': 25}
-        lot = creation_colis2(['enveloppe', 'petit', 'moyen', 'grand','enorme','petit','enveloppe','enorme'])
+        ascenceur = self.ascenceur
+        lot = self.lot
         prise = [prendre_colis(ascenceur, colis) for colis in lot]
-        self.assertEqual(prise, [True, True, True, True, True, True, True, False])
-        self.assertEqual(ascenceur["colis"], lot[:7])
+        self.assertEqual(prise, [True, True, True, True, True, True, True, False])   
 
     def test_poids_dans_l_ascenceur(self):
-        ascenceur = {'colis': [], 'poids_max': 25}
-        lot = creation_colis2(['enveloppe', 'petit', 'moyen', 'grand','enorme','petit','enveloppe','enorme'])
+        ascenceur = self.ascenceur
+        lot = self.lot
         prise = [prendre_colis(ascenceur, colis) for colis in lot]
         self.assertEqual(prise, [True, True, True, True, True, True, True, False])
-
         poids_dans_l_ascenceur(ascenceur)
         self.assertEqual(ascenceur["colis"], lot[:7])
-"""
+
     def test_ship(self):
-        categories, ascenceur = self.categorie, self.ascenceur
-        a1 = categories[:7]
+        categorie, ascenceur, lot = self.categorie, self.ascenceur, self.lot
+        a1 = categorie[:7]
+        poids = poids_dans_l_ascenceur(ascenceur)
         for categorie in a1:
             prendre_colis(categorie, ascenceur)
         restants = categorie[7:]
         prend_colis(restant[0], ascenceur)
-        self.assertEquals(ascenceur['categories'], [restant[0]])
-       """
-
+        self.assertEquals(ascenceur['colis'], [restant[0]])
+       
 
 if __name__ == "__main__":
     import fichier
