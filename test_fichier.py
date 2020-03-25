eb78d10798a19967079e402552408c2dd86891c4
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2020-03-25 14:29:36 +0100

    Update test_fichier.py

--- test_fichier.py
+++ test_fichier.py
@@ -1,4 +1,4 @@
-from fichier import categorie
+from fichier import prendre_colis,final_colis_poids
 
 class MyTestCase(unittest.TestCase):
     def test_prendre_colis(self):
@@ -31,6 +31,19 @@ class MyTestCase(unittest.TestCase):
     
         final_colis_poids(ascenceur)
         self.assertFalse(ascenceur['categories'])
+        
+    def setUp(self):
+        self.categorie = [creation_colis(categorie) for categorie in['enveloppe', 'petit', 'moyen', 'grand', 'enorme', 'petit', 'envloppe', 'enorme']] 
+        self.ascenceur = {'categories': [], 'poids_max': 25}
+        
+    def test_ship(self):
+        categories, ascenceur = self.categorie, self.ascenceur
+        a1 = categories[:7]
+        for categorie in a1:
+            prend_colis(categorie, ascenceur)
+        restants = categorie[7:]
+        prend_colis(restant[0], ascenceur)
+        self.assertEquals(ascenceur['categories'], [restant[0]])
         
 if __name__ == "__main__":
     import doctest
