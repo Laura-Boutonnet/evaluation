83ae6be80fe13fef7f9211f05e955d1e4bd45686
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2020-03-26 20:37:50 +0100

    Update controle_cours.py

--- controle_cours.py
+++ controle_cours.py
@@ -57,19 +57,13 @@ class Pays():
         self.nom = nom
         self.visa = visa
 
-
 ville_pays = {'Paris':'France','Berlin':'Allemagne','Madrid':'Espagne','Moscou':'Russie'        }
 
 if __name__ == "__main__": 
-
     import doctest 
-
     if True: 
-
         doctest.testmod(verbose=True, optionflags=512) 
-
     else: 
-
         doctest.testmod(verbose=True) 
 
 
