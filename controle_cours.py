5b293c887b2d6983de03e5a5ef34b3143434eada
Laura-Boutonnet <laura.boutonnet@hotmail.fr>
2020-03-26 18:45:53 +0000

    Controle

--- controle_cours.py
+++ controle_cours.py
@@ -1,78 +1,80 @@
-# q1 
+#q1
 
-def sup21(): 
-
-    i = 1
-    while i< 22:
-        print(i)
-        i+=1 
-
-sup21() 
+def sup21(nbr):
+    if nbr >= 21:
+        print(True)
+    else:
+        print(False)
 
 #q2
-
-def pairs(): 
-
-    nb = [ n**2 for n in NB if n%2 != 0] 
-
-    return nb
-
+def pairs(li):
+    return [nb for nb in li if nb % 2 == 0]
 
 #q3
-def ajout4(liste): 
+def ajout4(lis):
+    return lis + [4]
 
-    li = liste.append(4) 
+#q4
+def to_strings(dic):
+    liste = []
+    for a, b in dic.items():
+        liste.append(str(a) + ":" + str(b))
+    return liste
 
-    return li 
-
-#q4 
-def to_strings(di):
-  lis = []
-  for a,b in di.items():
-    for i in range(b):
-      lis.append(a)
-
-  return lis
 #q5
-def extremites(liste1):
-  li1 = liste1[c(1,2,-1,-2)]
-  return li1
+def extremites(liste2):
+    return liste[2:] + liste[-2:]
 
-#q6
-class Mot(): 
+class Mot():
+    def __init__ (self, mot):
+        self.mot = mot
+    def mot(self, mot):
+        self.mot = mot
+    def comptelettre(self, lettre, mot):
+        self.letter = lettre.lower
+        for lettre in mot:
+            if lettre.lower() in mot.lower():
+                print(self.count(lettre.lower()))
 
-    def __init__(self, mot, lettre): 
-        self.mot = mot 
-        self.lettre = mot_len(lettre) 
-    def comptelettre(self): 
-        return f"Il y a {self.lettre} mettre dans le mot ({self.mot})" 
 
-   
-
-#q7
-def tri_et_inverse(liste2):
-  lis2 = liste2.sort()
-  liste21 = liste2.reverse()
-  return lis2,liste21
-
-#q8
-  #je ne sais pas faire
+def tri_et_inverse(liste):
+    return (sorted(liste), reversed(liste))
+       
+        
+def aller_a_paris(input_call=input):
+    # code a remplir
+    while input_call.lower() != 'paris':
+        saisie = input_call('Question ')
+    return saisie
+    # quelque part dans le code de cette fonction: saisie = input_call('Question ')
+    # en fonction de saisie on continue a demander ou on renvoie 'Paris'
+    # Au lieu d'utiliser input comme en cours vous appelez input_call
+    # par dÃ©faut elle vaut input donc vous pouvez appeller
+    # aller_a_paris() pour tester a la main
+    while True:
+        return 0, 'Nulle Part'
   
 #q9
-
-ville_nom_pays = {'Paris':'France', 'Berlin':'Allemagne', 'Madrid':'Espagne', 'Moscou':'Russie'}
+ville_nom_pays = {
+        'Paris':'France',
+        'Berlin':'Allemagne',
+        'Madrid':'Espagne',
+        'Moscou':'Russie'
+        } 
 
 #q10
+class Pays():
+    def __init__ (self, nom, visa):
+        self.nom = nom
+        self.visa = visa
 
-class Pays:
-  nom = ['France', 'Allemagne', 'Espagne' , 'Russie']
-  visa = {'France':False,'Allemagne':False,'Espagne':False, 'Russie':True}
-  def __init__(self,nom,visa,ville_pays):
-    self.nom = nom
-    self.visa = visa
-    return f"{self.visa} and ({self.nom}) "
 
-ville_pays = {'Paris':False, 'France':False, 'Berlin':False, 'Allemagne':False, 'Madrid':False,'Espagne':False, 'Moscou':True, 'Russie':True}    
+ville_pays = {
+        'Paris':'France',
+        'Berlin':'Allemagne',
+        'Madrid':'Espagne',
+        'Moscou':'Russie'
+        }
 
 if __name__ == "__main__": 
 
